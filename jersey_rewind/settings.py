c14b2143c1d9ce591a897470dec5634f1874d3f7
Ben Kelly <benskelly10@gmail.com>
2021-11-03 00:30:59 +0000

    Add object params

--- jersey_rewind/settings.py
+++ jersey_rewind/settings.py
@@ -170,6 +170,9 @@ STATICFILES_DIRS = [
 ]
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
     AWS_STORAGE_BUCEKT_NAME = 'jersey-rewind'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
