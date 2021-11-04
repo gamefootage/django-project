3de8db5bc866855d3f073d478a094c71d47bc285
Ben Kelly <benskelly10@gmail.com>
2021-11-04 01:06:36 +0000

    Freeze requirements

--- products/views.py
+++ products/views.py
@@ -1,1 +1,14 @@
 """Configure views for products app"""
+from django.shortcuts import render
+from .models import Product
+
+
+def get_all_products(request):
+    """ View for getting all products """
+
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/products.html', context)
