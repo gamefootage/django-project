81a649c84c5677097820fe0b4d98386c286957cd
Ben Kelly <benskelly10@gmail.com>
2021-11-04 00:21:23 +0000

    Configure admin views for products and collections

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import Product, Collection
 
 
-admin.site.register(Product)
-admin.site.register(Collection)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'display_name',
+        'price',
+        'image_url',
+        'quantities'
+    )
+
+    ordering = ('pk',)
+
+    def quantities(self, obj):
+
+        return f'{obj.quantity["s"] + obj.quantity["m"] + obj.quantity["l"]} \
+            (s: {obj.quantity["s"]}, m: {obj.quantity["m"]}, l: {obj.quantity["l"]})'
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'display_name'
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Collection, CollectionAdmin)
